401190f81cc0b26802e59b48505dce303a447beb
Shuntaro Shimizu <ut.s.shimizu@gmail.com>
2021-04-25 14:32:23 +0900

    complete crawler

--- src/crawler.py
+++ src/crawler.py
@@ -25,7 +25,7 @@ class Crawler:
         status_table_bs = status_list_bs.find(
             "table", {"class": "center stupidtable stupidtable_common"})
         trs = status_table_bs.find_all("tr")
-        for tr in trs[1:]:
+        for tr in trs[1:]:  # 1番目の要素はカラム名であるためスキップ
             a = tr.find("a")
 
             detail_page_url = Settings.BASE_URL + a["href"]
@@ -33,7 +33,9 @@ class Crawler:
 
             no = tr.find("td", {"class": "c1"}).text
             file_name = "{}_{}.html".format(no, a.text)
-            if not "相棒" in a.text:
+            if a.text in Settings.EXCEPT_POKEMON_NAMES:
+                print("{} は除外されました。".format(a.text))
+            else:
                 Utils.write_html(detail_page_bs.prettify(),
                                  detail_page_dir_path, file_name)
 
